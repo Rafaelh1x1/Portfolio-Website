03e39eb89b5c221d341ed2d38b7a8afb7a4041aa
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-05-01 14:10:10 -0400

    makign javascript work with css

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const openNavEl = document.querySelector(".open-nav");
+const closeNavEl = document.querySelector(".close-nav");
+const miniSection = document.querySelector(".mini-section");
+
+openNavEl.addEventListener("click", function () {
+  openNavEl.style.display = "none";
+  closeNavEl.style.display = "block";
+  miniSection.style.display = "block";
+});
