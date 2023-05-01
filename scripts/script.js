ffecdec7093a64c5ba7aa516b37756e584ee85a1
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-05-01 15:11:48 -0400

    Mobile navigation

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,23 @@
 
 const openNavEl = document.querySelector(".open-nav");
 const closeNavEl = document.querySelector(".close-nav");
-const miniSection = document.querySelector(".mini-section");
+const miniSection = document.getElementById("mini-section");
+const miniSectionItem = document.querySelector(".mini-section");
 
 openNavEl.addEventListener("click", function () {
   openNavEl.style.display = "none";
   closeNavEl.style.display = "block";
-  miniSection.style.display = "block";
+  miniSection.style.display = "flex";
+  miniSection.style.marginBottom = "0";
+  miniSectionItem.classList.add("flicker-in-1");
+
+  console.log("poop");
+});
+
+closeNavEl.addEventListener("click", function () {
+  openNavEl.style.display = "block";
+  closeNavEl.style.display = "none";
+  miniSection.style.display = "none";
+  miniSectionItem.classList.remove("flicker-in-1");
+  console.log("poop");
 });
