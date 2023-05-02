a68636a82eeee59ba5eeb76d070249347164950b
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-05-02 08:41:58 -0400

    Animate on scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,27 @@
 "use strict";
 
+// DECLARE VARIABLES
+
 const openNavEl = document.querySelector(".open-nav");
 const closeNavEl = document.querySelector(".close-nav");
 const miniSection = document.getElementById("mini-section");
 const miniSectionItem = document.querySelector(".mini-section");
+const vw = document.body.clientWidth;
+// ANIMATE ON SCROLL
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("flicker-in-1");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// MOBILE NAVIGATION STUFF
 
 openNavEl.addEventListener("click", function () {
   openNavEl.style.display = "none";
@@ -39,3 +57,10 @@ closeNavEl.addEventListener("mouseout", function () {
   closeNavEl.style.color = "#416241";
   closeNavEl.style.backgroundColor = "#101810";
 });
+
+// SCREEN SIZE CHANGES
+// if (vw < 742) {
+//   openNavEl.classList.add("hidden");
+// } else if (vw > 742) {
+//   openNavEl.classList.remove("hidden");
+// }
